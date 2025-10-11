5edee160ac15b44ff06a19adc6f5ed0d93abd61d
James Martin <jtmar@cs.washington.edu>
2025-10-11 12:52:02 -0700

    Much cleaner rewrite.

--- Stacks/Basic.lean
+++ Stacks/Basic.lean
@@ -1,84 +1,72 @@
 import Mathlib.CategoryTheory.Category.Basic
-import Mathlib.CategoryTheory.Comma.Over.Basic
 import Mathlib.CategoryTheory.Iso
 import Mathlib.CategoryTheory.Limits.Shapes.Pullback.CommSq
 
 open CategoryTheory
 open CategoryTheory.Limits
 
-variable {T : Type} [Category T] {U V W : T}
+variable {C : Type} [Category C]
 
--- This is probably equivalent to Over U.
-structure To (U : T) where
-  V : T
-  f : V ⟶ U
+-- Workaround that probably has a better solution.
+inductive Lift (P : Prop) : Type where
+| lift (p : P)
 
--- This is probably in Mathlib, but I couldn't find it.
-structure PullbackOf (f : To U) (g : To U) where
-  pullback : T
-  fst : pullback ⟶ f.V
-  snd : pullback ⟶ g.V
+-- A morphism with destimation dst.
+structure To (dst : C) where
+  src : C
+  f : src ⟶ dst
+
+-- A pullback of f and g.
+structure Pullback {dst : C} (f : To dst) (g : To dst) where
+  obj : C
+  fst : obj ⟶ f.src
+  snd : obj ⟶ g.src
   is_pullback : IsPullback fst snd f.f g.f
 
-def pullback_snd {f g : To U} (square : PullbackOf f g)
-  := To.mk square.pullback square.snd
+-- A family or morphisms with destination dst.
+def Cover (dst : C) := Set (To dst)
 
--- A family of morphisms with target U in C.
-def Cover {T : Type} [Category T] (U : T) := Set (To U)
+-- A collection of covers for every object in the category.
+def Coverings (C : Type) [Category C] := ∀ (dst : C), Set (Cover dst)
 
--- Why TF is this necessary???
--- Shouldn't Cover U immediately reduce to Set (To U)?
-def coerce (cover : Cover U) : Set (To U) := cover
-instance : Membership (To U) (Cover U) where
-  mem (cover : Cover U) (f : To U) := f ∈ coerce cover
+-- Support `f ∈ cover` syntax. Since Cover is literally defined as a set,
+-- I have no idea why Lean isn't figuring this out automatically.
+def coerce_cover {dst : C} (cover : Cover dst) : Set (To dst) := cover
+instance {dst : C} : Membership (To dst) (Cover dst) where
+  mem cover dst := dst ∈ coerce_cover cover
 
-def Coverage (T : Type) [Category T] := ∀ {U : T}, Set (Cover U)
-
-structure CoverIn (coverage : Coverage T) where
-  obj : T
-  cover : Cover obj
-  in_coverage : cover ∈ coverage
-
-structure CoverObj (cover : Cover U) where
-  obj : T
-  f : obj ⟶ U
-  in_cover : To.mk obj f ∈ cover
-
--- A cover defined by a set of morphisms from a single source.
-def single_source_cover (M : Set (V ⟶ U)) : Cover U
-  := { f : To U | ∃ (p : f.V = V), match p with | Eq.refl f.V => f.f ∈ M }
-
-inductive Lift (P : Prop) : Type where
-| lift (p : P) : Lift P
-
-class Site (T : Type) extends Category T where
-  -- A set of covers for each object in C.
-  coverage : ∀ {U : T}, Set (Cover U)
-  -- Every isomorphism is a cover in itself.
-  identity : ∀ {U V : T} (iso : Iso V U),
-    single_source_cover { f : V ⟶ U | f = Iso.hom iso } ∈ coverage
-  -- Transitivity/composition of covers.
-  compose :
-    ∀ (cover : CoverIn coverage)
-    (pre_covers : ∀ (V : CoverObj cover.cover), Cover V.obj),
-    (∀ (V : CoverObj cover.cover), pre_covers V ∈ coverage) →
-    -- A morphism is in the composed cover if it is the composition
-    -- of a morphism in the cover {g : Wᵢⱼ → Vᵢ) and a morphism in the
-    -- cover {h : Vᵢ → U}.
-    { f : To cover.obj |
-      ∃ (h : CoverObj cover.cover) (g : f.V ⟶ h.obj),
-      pre_covers h (To.mk f.V g) →
-      f.f = CategoryStruct.comp g h.f } ∈ coverage
-  pullback_exists :
-    -- Unclear why Lean can't synthesize membership To U ∈ Cover U
-    -- given that Cover U is literally defined as Set (To U).
-    ∀ {U : T} {u_cover : Cover U}, u_cover ∈ coverage →
-    ∀ (g : To U),
-    ∀ {f : To U}, f ∈ u_cover →
-    PullbackOf f g
-  pullback_cover :
-    ∀ {U : T} (u_cover : Cover U) (u_in_coverage : u_cover ∈ coverage),
-    ∀ (g : To U),
-    { snd : To g.V |
-      ∃ (f : To U) (f_in_cover : f ∈ u_cover),
-      snd = pullback_snd (pullback_exists u_in_coverage g f_in_cover) } ∈ coverage
+class Site (C : Type) extends Category C where
+  coverings : Coverings C
+  -- Isomorphisms are covers unto themselves..
+  iso : ∀ (src dst : C) (iso : Iso src dst),
+        { f : To dst | f = To.mk src iso.hom } ∈ coverings dst
+  -- Given a cover of 'dst' and a cover for every object 'mid' in the domain of dst,
+  -- the composition of the morphisms of the covers forms a cover.
+  compose : ∀ dst : C, ∀ cover ∈ coverings dst,
+            -- A family of covers for every object in the domain of the cover.
+            ∀ (src : ∀ f ∈ cover, Σ cover', Lift (cover' ∈ coverings f.src)),
+            -- We're looking for morphisms that are the composition of morphisms in our covers.
+            { h : To dst |
+              -- A morphism in the destination cover.
+              ∃ f : To dst,
+              ∃ f_in_cover : f ∈ cover,
+              -- A morphism in the source cover.
+              ∃ g ∈ (src f f_in_cover).fst,
+              -- h is a composition of the morphisms.
+              h = To.mk g.src (CategoryStruct.comp g.f f.f)
+             } ∈ coverings dst
+  -- Pullbacks exist for morphisms in a cover with any other morphism in the category.
+  pullback :
+    ∀ {dst : C},
+    ∀ g : To dst,
+    ∀ cover ∈ coverings dst, ∀ f ∈ cover,
+    Pullback f g
+  -- The pullbacks of g with respect to a cover of dst cover the source of g.
+  pullbacks_cover :
+    ∀ {dst : C}, ∀ g : To dst,
+    ∀ (cover : Cover dst) (cover_in : cover ∈ coverings dst),
+    { h : To g.src
+    | ∀ (f : To dst) (f_in : f ∈ cover),
+      let pullback := pullback g cover cover_in f f_in
+      h = To.mk pullback.obj pullback.snd
+    } ∈ coverings g.src
