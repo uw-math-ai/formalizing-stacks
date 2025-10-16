67eec5cc1467e95f9894dbf974c55dd2fe48e6d7
Dowland Aiello <dowlandaiello@gmail.com>
2025-10-16 12:22:37 -0700

    Canonical functor lemma

--- Stacks/SiteDef2.lean
+++ Stacks/SiteDef2.lean
@@ -1,9 +1,14 @@
 import Mathlib.CategoryTheory.Category.Basic
 import Mathlib.CategoryTheory.Iso
 import Mathlib.CategoryTheory.Limits.Shapes.BinaryProducts
+import Mathlib.CategoryTheory.Sites.Precoverage
 
 open CategoryTheory (Category)
+open CategoryTheory (asIso)
 open CategoryTheory (Iso)
+open CategoryTheory (IsIso)
+open CategoryTheory (Precoverage)
+open CategoryTheory.Limits
 
 /-
 A definition of a site that attempts to use as much from mathlib as possible.
@@ -11,10 +16,8 @@ I also attempt to use encodings more native to type theory.
 I think this definition is quite concise, but let me know if anything is wrong.
 -/
 
-namespace CategoryTheory
-
 inductive Site.Covering.{v, u} {C : Type u}
-  [Category.{v, u} C] [Limits.HasBinaryProducts.{v, u} C] :
+  [Category.{v, u} C] [HasBinaryProducts.{v, u} C] :
   C → C → Prop
   -- Axiom 1
   | iso   (X Y      : C) : Iso X Y → Site.Covering X Y
@@ -25,12 +28,20 @@ inductive Site.Covering.{v, u} {C : Type u}
   -- Axiom 3
   | prod (X Y Z XZ  : C) : Site.Covering Y Z
     → Nonempty (X ⟶ Z)
-    → Limits.HasBinaryProduct Y XZ
-    → Site.Covering (Limits.prod Y XZ) X
+    → HasBinaryProduct Y XZ
+    → Site.Covering (prod Y XZ) X
 
 structure Site.{v, u} {C : Type u}
-  [Category.{v, u} C] [Limits.HasBinaryProducts.{v, u} C]
+  [Category.{v, u} C] [HasBinaryProducts.{v, u} C]
   (Z : C) where
   coverings (X : C) : (X ⟶ Z) → Site.Covering X Z
 
-end CategoryTheory
+/-
+Example 3 from Stacks project page (https://stacks.math.columbia.edu/tag/00VG):
+-/
+
+lemma canonical_functor.{v, u} {C : Type u} [Category.{v, u} C] [HasBinaryProducts.{v, u} C]
+  (X : C) : (∀ (Y : C) (hom : Y ⟶ X), IsIso hom) → @Site.{v, u} C _ _ X := fun h_isos => {
+    coverings Y := fun hom =>
+      Site.Covering.iso _ _ (@asIso _ _ _ _  hom (h_isos Y hom))
+  }
