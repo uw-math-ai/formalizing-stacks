021e6659b0119b2caa19a6642e0eb43f3738f8d3
Dowland Aiello <dowlandaiello@gmail.com>
2025-10-17 13:09:31 -0700

    Progress on XZar binary products

--- Stacks/SiteDef2.lean
+++ Stacks/SiteDef2.lean
@@ -62,18 +62,60 @@ def Hom.{u} {C : Type u} [TopologicalSpace C] (X Y : @Obj C _) := X.x → Y.x
 instance Hom.instQuiver.{u} {C : Type u} [TopologicalSpace C] : Quiver.{u + 1} (@Obj.{u} C _) where
   Hom X Y := Hom X Y
 
-instance instCategoryXZar.{u} {C : Type u} [TopologicalSpace C] [Quiver (@Obj C _)]
+instance instCategoryXZar.{u} {C : Type u} [TopologicalSpace C] [Quiver.{u + 1, u} (@Obj C _)]
   : Category.{u} (@Obj C _) where
   Hom X Y := Hom X Y
   id X := id
   comp hom_xy hom_xz := hom_xz ∘ hom_xy
 
-structure Prod.{v} {C : Type v} [TopologicalSpace C] [h : Category.{v + 1, v} (@Obj.{v} C _)]
+structure Prod.{v} {C : Type v} [TopologicalSpace C] [Category.{v + 1, v} (@Obj.{v} C _)]
   (X Y : @Obj.{v} C _) where
-  P        : @Obj.{v} C _ := { x := X.x ∩ Y.x, h_open := IsOpen.inter X.h_open Y.h_open }
+  P : @Obj.{v} C _ := { x := X.x ∩ Y.x, h_open := IsOpen.inter X.h_open Y.h_open }
+  p_hom_def_eq : P.x = X.x ∩ Y.x := by
+    simp
   π₁       : P ⟶ X
   π₂       : P ⟶ Y
   is_limit : IsLimit (@BinaryFan.mk.{v, v} (@Obj C _) _ X Y P π₁ π₂)
+
+def Prod.mk'.{v} {C : Type v} [TopologicalSpace.{v} C] [Category.{v + 1, v} (@Obj.{v} C _)] (X Y : @Obj.{v} C _): Prod.{v} X Y :=
+  let π₁ := fun inter =>
+    by
+      simp at inter
+      let { val := val_inter, property := h_inter } := inter
+      have h_inclusion : val_inter ∈ X.x := Set.mem_of_mem_inter_left h_inter
+      have h_coe : Subtype X.x := { val := val_inter, property := (by assumption) }
+      exact h_coe
+  let π₂ := fun inter =>
+    by
+      simp at inter
+      let { val := val_inter, property := h_inter } := inter
+      have h_inclusion : val_inter ∈ Y.x := Set.mem_of_mem_inter_right h_inter
+      have h_coe : Subtype Y.x := { val := val_inter, property := (by assumption) }
+      exact h_coe
+
+  {
+    π₁ := π₁
+    π₂ := π₂
+    is_limit := IsLimit.mk
+      (fun s x => (by
+        simp
+        match h : s with
+        | .mk x₀ π =>
+          match π with
+          | ⟨mpr, mpl⟩ =>
+            let pair₁ : CategoryTheory.Discrete WalkingPair := { as := WalkingPair.left }
+            let pair₂ : CategoryTheory.Discrete WalkingPair := { as := WalkingPair.right }
+            let val_in_pair₁ := π.app pair₁ (by assumption)
+            let val_in_pair₂ := π.app pair₂ (by assumption)
+            let { val := val₁, property } := val_in_pair₁
+            let { val := val₂, property := prop₂ } := val_in_pair₂
+            have h_left : val₁ ∈ X.x := property
+            have h_right : val₂ ∈ Y.x := prop₂
+            exact ⟨val₁, ⟨h_left, h_right⟩⟩
+            ))
+      sorry
+      sorry
+  }
 
 instance instHasBinaryProductsXZar.{u} {C : Type u} [TopologicalSpace C] [Quiver (@Obj C _)] [Category (@Obj C _)] :
   HasBinaryProducts (@Obj C _) where
