a537c201e6b680adba9d0335b8c46739b81c5576
Alexandra Aiello <dowlandaiello@gmail.com>
2025-11-09 23:00:09 -0800

    Clean

--- Stacks/Sheaves.lean
+++ Stacks/Sheaves.lean
@@ -11,18 +11,10 @@ abbrev Presheaf.{u, v} (C : Type v) [Category.{u, v} C]:=
 def Sheaf.{u, v} (C : Type v)
   [Category.{u, v} C] [HasPullbacks C] [h_site : Site'.{u, v} C]
   (F : Presheaf.{u, v} C) := ∀ (U : C) (pre : Presieve U)
-    (in_cov : pre ∈ h_site.coverings U)
-    (Uj Uk K : C)
-    (p_j : Uj ⟶ U)
-    (p_k : Uk ⟶ U)
-    (f : K ⟶ Uj)
-    (g : K ⟶ Uk)
-    (in_cov_j : p_j ∈ @pre Uj)
-    (in_cov_k : p_k ∈ @pre Uk)
-    (comp_eq : f ≫ p_j = g ≫ p_k)
+    (_in_cov : pre ∈ h_site.coverings U)
     (fam : FamilyOfElements F pre)
-    (fam_eq : F.map (.op f) (fam p_j in_cov_j) =
-      F.map (.op g) (fam p_k in_cov_k)),
-    ∀ (Ui : C)
-    (hom : Ui ⟶ U)
-    (in_cov : hom ∈ @pre Ui), ∃! s : (F.obj (.op U)), fam hom in_cov = F.map (.op hom) s
+    (_comp : fam.Compatible),
+    ∃! s : (F.obj (.op U)),
+      ∀ (Ui : C)
+      (hom : Ui ⟶ U)
+      (in_cov : hom ∈ @pre Ui), fam hom in_cov = F.map (.op hom) s
