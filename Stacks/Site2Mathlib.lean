bd3e567c0da8ecba58b0eb9abf3a21adec4417c8
Alexandra Aiello <dowlandaiello@gmail.com>
2025-11-11 01:22:02 -0800

    Recover pretopology from Site'

--- Stacks/Site2Mathlib.lean
+++ Stacks/Site2Mathlib.lean
@@ -25,4 +25,23 @@ def of_pretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} 
     simp
   pullback f U h_precov_is_cov := pre.pullbacks f U h_precov_is_cov
 
+def to_pretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} C]
+  (s : Site' C) : Pretopology C where
+  coverings := s.coverings
+  has_isos X Y f h_iso := @s.iso X Y f h_iso
+  pullbacks X Y f cov is_cov := @s.pullback X Y f cov is_cov
+  transitive X U R is_cov h := @s.trans X U is_cov
+    (fun Y f in_cov => { val := @R Y f in_cov, property := @h Y f in_cov })
+
 end Site'
+
+namespace GrothendieckTopology
+
+def of_site'.{u, v} {C : Type v} [h_cat : Category.{u, v} C] [HasPullbacks.{u, v} C] (s : Site' C) : GrothendieckTopology.{u, v} C where
+  sieves X := { sieve | sieve.arrows ∈ s.coverings X }
+  top_mem' X := by
+    simp
+    
+    sorry
+
+end GrothendieckTopology
