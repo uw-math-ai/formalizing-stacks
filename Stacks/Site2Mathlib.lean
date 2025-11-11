3501e7a50f2fdeef9d81107cea5baf72f175b5e3
Alexandra Aiello <dowlandaiello@gmail.com>
2025-11-11 00:12:30 -0800

    of_pretopology helper

--- Stacks/Site2Mathlib.lean
+++ Stacks/Site2Mathlib.lean
@@ -12,3 +12,17 @@ class Site'.{u, v} (C : Type v) [Category.{u, v} C] [HasPullbacks.{u, v} C] wher
       Presieve.bind U (fun {Y : C} {_f : Y ⟶ X} h => (R h).val) ∈ coverings X
   pullback {X Y : C} (f : Y ⟶ X) (U : Presieve X) (hU : U ∈ coverings X) :
     Presieve.pullbackArrows f U ∈ coverings Y
+
+namespace Site'
+
+def of_pretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} C]
+  (pre : Pretopology C) : Site' C where
+  coverings := pre.coverings
+  iso {X Y} f h_iso := pre.has_isos f
+  trans {X} precov h_is_precov mk_left_cov := by
+    apply Pretopology.transitive pre precov
+    · exact h_is_precov
+    simp
+  pullback f U h_precov_is_cov := pre.pullbacks f U h_precov_is_cov
+
+end Site'
