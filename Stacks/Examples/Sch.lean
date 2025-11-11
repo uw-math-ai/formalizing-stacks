4e9a86b3d570eef6490e4742d19dcb8076e453ab
Alexandra Aiello <dowlandaiello@gmail.com>
2025-11-11 02:21:52 -0800

    Done

--- Stacks/Examples/Sch.lean
+++ Stacks/Examples/Sch.lean
@@ -15,15 +15,7 @@ instance instSubcanonical.{u} : GrothendieckTopology.Subcanonical instSite.{u}.t
   simp [instSite, Site'.toGrothendieck, ← Scheme.zariskiTopology_eq]
   exact Scheme.subcanonical_zariskiTopology
 
-def presheaf.{u} (S : Scheme.{u}) : Presheaf Scheme.{u} (Type u)  :=
-  (sheafToPresheaf.{u, u, u + 1, u + 1} instSite.{u}.toGrothendieck _).obj
-    <| (instSite.{u}.toGrothendieck.yoneda.obj S)
-
-theorem sheaf.{u} (S : Scheme.{u}) : IsSheaf instSite (presheaf S) := by
-  intro U pre pre_is_cov fam fam_compatible
-  -- The zariski topology is subcanonical
-  have h := Sheaf.isSheaf_yoneda_obj S
-  
-  sorry
+def sheaf.{u} (S : Scheme.{u}) : Sheaf (Site'.toGrothendieck instSite.{u}) (Type u) :=
+  instSite.{u}.toGrothendieck.yoneda.obj S
 
 end Sch
