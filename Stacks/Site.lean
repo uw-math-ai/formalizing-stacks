e80ead1ded51d73b3c9d04c4639ea6dc47e892e1
Alexandra Aiello <dowlandaiello@gmail.com>
2025-11-17 15:59:52 -0800

    New coverings for James' Site to mathlib

--- Stacks/Site.lean
+++ Stacks/Site.lean
@@ -15,13 +15,24 @@ structure Site.{u, v} (C : Type v) [Category.{u, v} C] [HasPullbacks.{u, v} C] w
   pullback {X : C} (f : Over X) (U : Precover X) (hU : U ∈ coverings X) :
     { Over.mk (pullback.snd g.hom f.hom) | g ∈ U } ∈ coverings f.left
 
+namespace Precover
+
+@[simp]
+def to_presieve.{u, v} {C : Type v} [Category.{u, v} C] (X : C) (p : Precover X) : Presieve X :=
+  fun ⦃Y : C⦄ => { f : Y ⟶ X | Over.mk f ∈ p }
+
+@[simp]
+def of_presieve.{u, v} {C : Type v} [Category.{u, v} C] (X : C) (P : Presieve X) : Precover X :=
+  { ov | ov.hom ∈ @P ov.left }
+
+end Precover
+
 namespace Site
 
 def toPretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} C]
   (S : Site C) : Pretopology C where
-  -- There should be an easier way to do this, but we can't extract a hom easily from over
-  -- with the right type
-  coverings X := (fun precov => fun ⦃Y⦄ => fun hom => Over.mk hom ∈ precov) '' (S.coverings X)
+  -- A presieve is in the coverings if and only if it is in the pretopology
+  coverings X := { cov | ∃ pre ∈ S.coverings X, cov = pre.to_presieve }
   has_isos {X Y} f is_iso := by
     use {Over.mk f}
     constructor
@@ -32,41 +43,44 @@ def toPretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} C
       constructor
       intro h
       cases h
-      simp
+      change Over.mk f = Over.mk f
+      rfl
       intro h
       cases h
-      rfl
+      simp
   pullbacks X {Y} f precov precov_is_cov := by
     simp_all
     obtain ⟨x, in_cov, precov_eq⟩ := precov_is_cov
     have h_in_cov_y := S.pullback (Over.mk f) x in_cov
     simp at h_in_cov_y
     use {x_1 | ∃ g ∈ x, Over.mk (pullback.snd g.hom f) = x_1}
-    simp [h_in_cov_y]
+    constructor
+    exact h_in_cov_y
+    cases precov_eq
     funext
-    case h.h.h Z g =>
-      simp
+    case h.right.refl.h.h Z g =>
+      simp [Precover.to_presieve]
       constructor
       intro h
-      obtain ⟨g', in_precov, ov_eq⟩ := h
-      cases ov_eq
-      apply Presieve.pullbackArrows.mk
-      rw [← precov_eq]
-      simp
-      change g' ∈ x
-      exact in_precov
-      intro h
+      change ∃ g' ∈ x, Over.mk (pullback.snd g'.hom f) = Over.mk g
+      unfold Precover.to_presieve at h
       cases h
-      cases precov_eq
-      case mpr.mk.refl Z h hRh =>
-        exists Over.mk h
+      case mp.mk Z g h =>
+        exists Over.mk g
+      case mpr =>
+        intro h
+        obtain ⟨w, ⟨w_in_x, pullback_ov_eq⟩⟩ := h
+        cases pullback_ov_eq
+        unfold Precover.to_presieve
+        apply Presieve.pullbackArrows.mk
+        exact w_in_x
   transitive {X} presieve presieve_Y is_cov mk_cov := by
     simp_all
     obtain ⟨cov, is_cov, h_pre_eq⟩ := is_cov
     let h_trans := S.trans cov is_cov (fun ov in_cov => by
       let h : ∃ cover : Precover ov.left, cover ∈ S.coverings ov.left := by
         have h := mk_cov ov.hom (by
-          rw[← h_pre_eq]
+          rw [← h_pre_eq]
           simp
           change ov ∈ cov
           exact in_cov
@@ -78,30 +92,8 @@ def toPretopology.{u, v} {C : Type v} [Category.{u, v} C] [HasPullbacks.{u, v} C
       exists h'
       exact Classical.choose_spec h
     )
-    simp at h_trans
-    unfold Presieve.bind
-    simp
-    exists cov
-    constructor
-    exact is_cov
-    simp_all
-    funext
-    case right.h.h Z g =>
-      simp [← h_pre_eq]
-      constructor
-      intro h
-      let h' : presieve g := by
-        rw [← h_pre_eq]
-        simp
-        exact h
-      have h_cov_g := mk_cov g h'
-      constructor
-      constructor
-      use g
-      constructor
-      use h
-      
-      sorry
+    
+    sorry
 
 end Site
 
