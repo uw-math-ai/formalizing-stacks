eebedb0faf4c14f806d02df651db30029c71a7d6
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-10 06:03:34 -0800

    Surjective families 2 (#8)
    * Surjective families site WIP (not using tactics)
    
    * Surjective families site iso and trans
    
    * Surjective families for easier definition of HasPullbacks
    
    * Remove orphaned examples
    
    * Update lean toolchain, move James' surjective families.
    
    * Naturality proof
    
    * IsLimit lift
    
    * Going to extract out some theorems
    
    * IsLimit.fac
    
    * Shorten fact proof a little
    
    * ItLimit.uniq
    
    * HasPullbacks for surjective families
    
    * Surjective families site iso case
    
    * Almost done
    
    * Working on pullback proof
    
    * Need to split up surjective families
    
    * SurjectiveFamilies folder
    
    * Moving properties of Site out into their own theorems
    
    * Have recovered a functor from walkingcospan, can now use that it is a pullback
    
    * Modularizing My_F functor recover
    
    * Progress
    
    * Surjective families proof done
    
    * Done
    
    * Move all to SurjectiveFamilies.lean
    
    ---------
    
    Co-authored-by: James Martin <jtmar@cs.washington.edu>

--- Stacks/Examples/SurjectiveFamilies.lean
+++ Stacks/Examples/SurjectiveFamilies.lean
@@ -3,162 +3,59 @@ import Mathlib.CategoryTheory.ConcreteCategory.Basic
 import Mathlib.CategoryTheory.Types.Basic
 import Mathlib.CategoryTheory.Limits.HasLimits
 import Stacks.Site
+import Mathlib.CategoryTheory.Limits.Types.Pullbacks
 
 open CategoryTheory
 open CategoryTheory.Limits
+open CategoryTheory.Limits.limit
 
 abbrev JointlySurjective.{u} {Y : Type u} (U : Precover Y)
   := ∀ y : Y, ∃ f ∈ U, ∃ x : f.left, y = f.hom x
 
 namespace JointlySurjective
 
-namespace HasPullbacks
+def coverings.{u} (X : Type u) : Set (Precover X) := setOf JointlySurjective
 
-@[simp]
-def cone.pt.t_p.{u} (F : WalkingCospan ⥤ Type u) : Type u :=
-  (F.obj .left) × (F.obj .right)
-
-@[simp]
-def cone_pt.{u} (F : WalkingCospan ⥤ Type u) : Type u := {
-  p : cone.pt.t_p F // F.map WalkingCospan.Hom.inl p.fst = F.map WalkingCospan.Hom.inr p.snd }
-
-@[simp]
-def π.app.{u} (F : WalkingCospan ⥤ Type u) (span : WalkingCospan) :
-  ((Functor.const WalkingCospan).obj (cone_pt F)).obj span ⟶ F.obj span :=
-  match span with
-  | .left => fun X => by
-    simp at X
-    exact X.val.fst
-  | .right => fun X => by
-    simp at X
-    exact X.val.snd
-  | .one => by
-    simp only [Functor.const_obj_obj]
-    exact (fun X => X.val.fst) ≫ F.map WalkingCospan.Hom.inl
-
-@[simp]
-def π.naturality.{u} (F : WalkingCospan ⥤ Type u)
-  ⦃X Y : WalkingCospan⦄
-  (f : X ⟶ Y) :
-  ((Functor.const WalkingCospan).obj (cone_pt F)).map f ≫ π.app F Y = π.app F X ≫ F.map f := by
-  unfold π.app at *
-  change WalkingCospan.Hom _ _ at f
-  simp only [Functor.const_obj_obj, Functor.const_obj_map, Category.id_comp]
-  cases f
-  · rw [CategoryTheory.Limits.WidePullbackShape.hom_id]
-    simp only [CategoryTheory.Functor.map_id, Category.comp_id]
-  case term j =>
-    cases j
-    · rfl
-    ext a
-    let f_eq : F.map WalkingCospan.Hom.inl a.val.fst = F.map WalkingCospan.Hom.inr a.val.snd :=
-      a.property
-    simp only [cone_pt, cone.pt.t_p, id_eq, types_comp_apply, f_eq]
-
-@[simp]
-def cone.{u} (F : WalkingCospan ⥤ Type u) : Cone F where
-  pt := cone_pt F
-  π := {
-    app := π.app F
-    naturality := π.naturality F
-  }
-
-@[simp]
-def isLimit.lift.{u} (F : WalkingCospan ⥤ Type u) (s : Cone F) (point : s.pt) : (cone F).pt :=
-  match s with
-  | .mk pt' π' => by
-    let map_left := π'.naturality WalkingCospan.Hom.inl
-    let map_right := π'.naturality WalkingCospan.Hom.inr
-    simp at map_left
-    simp only [Functor.const_obj_obj, Functor.const_obj_map,
-      map_left, Category.id_comp] at map_right
-    refine ⟨⟨π'.app .left point, π'.app .right point⟩, ?_⟩
-    change (π'.app WalkingCospan.left ≫ F.map WalkingCospan.Hom.inl) point  =
-           (π'.app WalkingCospan.right ≫ F.map WalkingCospan.Hom.inr) point
-    rw [map_right]
-
-@[simp]
-def isLimit.fac.{u} (F : WalkingCospan ⥤ Type u) (s : Cone F)
-  (span : WalkingCospan) : isLimit.lift F s ≫ (cone F).π.app span = s.π.app span := by
-  simp only [cone, cone_pt, cone.pt.t_p, Functor.const_obj_obj, π.app, id_eq]
-  cases span
-  · ext a
-    change (s.π.app WalkingCospan.left ≫ F.map WalkingCospan.Hom.inl) a = s.π.app none a
-    rw [← s.π.naturality WalkingCospan.Hom.inl]
-    simp only [Functor.const_obj_obj, Functor.const_obj_map, types_comp_apply, types_id_apply]
-  case some val =>
-    cases val
-    repeat (ext; simp)
-
-def isLimit.uniq.{u} (F : WalkingCospan ⥤ Type u) (s : Cone F) (m : s.pt ⟶ (cone F).pt)
-  (h : (∀ (j : WalkingCospan), m ≫ (cone F).π.app j = s.π.app j)) : m = isLimit.lift F s := by
-  ext a
-  simp only [lift, cone]
-  conv =>
-    right
-    left
-    left
-    rw [← h .left]
-    rfl
-  conv =>
-    right
-    left
-    right
-    rw [← h .right]
-    rfl
-  rfl
-
-def has_limit.{u} : ∀ (x : WalkingCospan ⥤ Type u), HasLimit x := fun F =>
-  ⟨⟨{
-      cone := cone F
-      isLimit := {
-        lift := isLimit.lift F
-        fac := isLimit.fac F
-        uniq := isLimit.uniq F
-      }
-  }⟩⟩
-
-end HasPullbacks
-
-instance instHasPullbacks.{u} : HasPullbacks (Type u) where
-  has_limit := HasPullbacks.has_limit
-
-def SurjectiveFamiliesSite.{u} : Site (Type u) := {
-  coverings X := setOf JointlySurjective
-  iso {X Y : Type u} (f : Y ⟶ X) (hf : IsIso f) x :=
-    ⟨Over.mk f, ⟨by simp, inv f x, by
+def iso.{u} {X Y : Type u} (f : Y ⟶ X) (hF : IsIso f) (x : X) :
+  ∃ (f_1 : Over X), f_1 ∈ ({Over.mk f} : Set (Over X)) ∧ ∃ x_1, x = f_1.hom x_1 :=
+  ⟨Over.mk f, ⟨Set.mem_singleton _, inv f x, by
       change x = (inv f ≫ f) x
       rw [(CategoryTheory.inv_comp_eq_id f).mpr]
       repeat rfl
     ⟩⟩
-  trans {X} U U_jointly_surjective V x := by
-    simp only [Functor.const_obj_obj,
-      Functor.id_obj, exists_prop, Set.mem_setOf_eq, ↓existsAndEq, and_true,
-      Over.mk_left, Over.mk_hom, types_comp_apply]
+
+def trans.{u} ⦃X : Type u⦄ (U : Precover X)
+  (U_jointly_surjective : U ∈ coverings X)
+  (V : ∀ f ∈ U, ∃ cover, cover ∈ coverings f.left) (x : X) :
+  ∃ f cov g, (f ∈ U ∧ cov ∈ coverings f.left ∧ g ∈ cov) ∧ ∃ x_1, x = f.hom (g.hom x_1) := by
     let ⟨f, f_in_U, x, y_is_f_x⟩ := U_jointly_surjective x
     let ⟨cov, is_joint_surj⟩ := V f f_in_U
     let ⟨g, in_cov, g_left, heq⟩ := is_joint_surj x
-    exact ⟨f, cov, g, ⟨f_in_U, is_joint_surj, in_cov⟩, ⟨g_left, by simp_all⟩⟩
-  pullback {X} f U U_jointly_surjective y := by
-    obtain ⟨g, g_in_U, x, f_y_is_g_x⟩ := U_jointly_surjective (f.hom y)
-    simp at U_jointly_surjective
-    change U_jointly_surjective g at g_in_U
-    
-    have h := g_in_U
-    simp only [Functor.id_obj, Functor.const_obj_obj, Set.mem_setOf_eq,
-      exists_exists_and_eq_and, Over.mk_left, Over.mk_hom]
-    refine ⟨g, g_in_U, ?_⟩
+    exact ⟨f, cov, g, ⟨f_in_U, is_joint_surj, in_cov⟩, ⟨g_left, by rw [← heq]; exact y_is_f_x⟩⟩
 
-    let left : pullback g.hom f.hom ⟶ g.left := pullback.fst g.hom f.hom
-    let right : pullback g.hom f.hom ⟶ f.left := pullback.snd g.hom f.hom
+def pullback.{u} {X : Type u} (f : Over X) (U : Precover X)
+  (U_jointly_surjective : U ∈ coverings X) (y : f.left) :
+  ∃ f_1 ∈ {x | ∃ g ∈ U, Over.mk (pullback.snd g.hom f.hom) = x}, ∃ x, y = f_1.hom x := by
+  obtain ⟨g, g_in_U, x, f_y_is_g_x⟩ := U_jointly_surjective (f.hom y)
+  use Over.mk <| pullback.snd g.hom f.hom
+  simp only [Functor.id_obj, Functor.const_obj_obj, Set.mem_setOf_eq, Over.mk_left, Over.mk_hom]
+  let mk_pb : X ⟶ Limits.pullback g.hom f.hom := pullback.lift (Function.const _ x)
+    (Function.const _ y) (by
+      ext
+      exact f_y_is_g_x.symm
+    )
+  constructor
+  · use g
+  use mk_pb (f.hom y)
+  simp [mk_pb]
 
-    let hom_y_to_hom_x : f.left ⟶ g.left := fun e => by
-      
-      g.hom e
-    let hom_x_to_hom_y : g.left ⟶ f.left := fun e => y
-
-    let lifted : pullback g.hom f.hom := @pullback.lift _ _ _ _ _ _ g.hom f.hom _ (hom_y_to_hom_x) (CategoryStruct.id _) (by unfold hom_y_to_hom_x; simp; sorry)
-    sorry
-}
+instance instSite.{u} : Site (Type u) where
+  coverings := coverings
+  iso := iso
+  trans {X} U U_jointly V x := by
+    simp only [Functor.const_obj_obj, Functor.id_obj, exists_prop,
+      Set.mem_setOf_eq, ↓existsAndEq, and_true, Over.mk_left, Over.mk_hom, types_comp_apply]
+    exact trans U U_jointly V x
+  pullback := pullback
 
 end JointlySurjective
