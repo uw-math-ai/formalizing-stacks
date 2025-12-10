4ce841b34011c1af6e34156d067512b42b0fb76d
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-09 21:05:54 -0800

    Remove orphaned examples

--- Stacks/Site.lean
+++ Stacks/Site.lean
@@ -6,13 +6,6 @@ open CategoryTheory.Limits
 -- A family or morphisms with destination dst.
 abbrev Precover.{u, v} {C : Type v} [Category.{u, v} C] (X : C) := Set (Over X)
 
-structure Pullback.{u, v} {C : Type v} [Category.{u, v} C]
-          {dst : C} (f : Over dst) (g : Over dst) where
-  obj : C
-  fst : obj ⟶ f.left
-  snd : obj ⟶ g.left
-  is_pullback : IsPullback fst snd f.hom g.hom
-
 structure Site.{u, v} (C : Type v) [Category.{u, v} C] [HasPullbacks.{u, v} C] where
   coverings (X : C) : Set (Precover X)
   iso {X Y : C} (f : Y ⟶ X) : IsIso f → { Over.mk f } ∈ coverings X
