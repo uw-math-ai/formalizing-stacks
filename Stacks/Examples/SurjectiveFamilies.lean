fed3d3ec7728882cba53778a072b2243c469f807
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-10 02:18:11 -0800

    Working on pullback proof

--- Stacks/Examples/SurjectiveFamilies.lean
+++ Stacks/Examples/SurjectiveFamilies.lean
@@ -125,12 +125,12 @@ instance instHasPullbacks.{u} : HasPullbacks (Type u) where
 
 def SurjectiveFamiliesSite.{u} : Site (Type u) := {
   coverings X := setOf JointlySurjective
-  iso {X Y : Type u} (f : Y ⟶ X) (hf : IsIso f) x := by
-    exists Over.mk f
-    refine ⟨by simp, inv f x, by
+  iso {X Y : Type u} (f : Y ⟶ X) (hf : IsIso f) x :=
+    ⟨Over.mk f, ⟨by simp, inv f x, by
       change x = (inv f ≫ f) x
       rw [(CategoryTheory.inv_comp_eq_id f).mpr]
-      repeat rfl⟩
+      repeat rfl
+    ⟩⟩
   trans {X} U U_jointly_surjective V x := by
     simp only [Functor.const_obj_obj,
       Functor.id_obj, exists_prop, Set.mem_setOf_eq, ↓existsAndEq, and_true,
@@ -138,15 +138,21 @@ def SurjectiveFamiliesSite.{u} : Site (Type u) := {
     let ⟨f, f_in_U, x, y_is_f_x⟩ := U_jointly_surjective x
     let ⟨cov, is_joint_surj⟩ := V f f_in_U
     let ⟨g, in_cov, g_left, heq⟩ := is_joint_surj x
-    refine ⟨f, cov, g, ⟨f_in_U, is_joint_surj, in_cov⟩, (by use g_left; simp_all)⟩
-  pullback f U U_jointly_surjective y := by
-    obtain ⟨ g, g_in_U, x, f_y_is_g_x ⟩ := U_jointly_surjective (f.hom y)
-    exists Over.mk pullback.snd g.hom f.hom
-    apply And.intro
-    · exists g
-    · exists by
-        exists (x, y)
-        rw [f_y_is_g_x]
+    exact ⟨f, cov, g, ⟨f_in_U, is_joint_surj, in_cov⟩, ⟨g_left, by simp_all⟩⟩
+  pullback {X} f U U_jointly_surjective y := by
+    obtain ⟨g, g_in_U, x, f_y_is_g_x⟩ := U_jointly_surjective (f.hom y)
+    simp only [Functor.id_obj, Functor.const_obj_obj, Set.mem_setOf_eq,
+      exists_exists_and_eq_and, Over.mk_left, Over.mk_hom]
+    refine ⟨g, g_in_U, ?_⟩
+
+    let hom_y_to_hom_x : f.left ⟶ g.left := fun e => x
+    let hom_x_to_hom_y : g.left ⟶ f.left := fun e => y
+
+    let left : pullback g.hom f.hom → g.left := pullback.fst g.hom f.hom
+    let right : pullback g.hom f.hom → f.left := pullback.snd g.hom f.hom
+
+    let lifted : pullback g.hom f.hom := pullback.lift (by sorry)
+    sorry
 }
 
 end JointlySurjective
