54b7e775d42a9fb503eb41fac9bea59dfedb3156
Alexandra Aiello <dowlandaiello@gmail.com>
2025-12-10 05:42:34 -0800

    Done

--- Stacks/Examples/SurjectiveFamilies/Site.lean
+++ Stacks/Examples/SurjectiveFamilies/Site.lean
@@ -31,26 +31,29 @@ def trans.{u} ⦃X : Type u⦄ (U : Precover X)
     let ⟨g, in_cov, g_left, heq⟩ := is_joint_surj x
     exact ⟨f, cov, g, ⟨f_in_U, is_joint_surj, in_cov⟩, ⟨g_left, by rw [← heq]; exact y_is_f_x⟩⟩
 
-def SurjectiveFamiliesSite.{u} : Site (Type u) := {
+def pullback.{u} {X : Type u} (f : Over X) (U : Precover X)
+  (U_jointly_surjective : U ∈ coverings X) (y : f.left) :
+  ∃ f_1 ∈ {x | ∃ g ∈ U, Over.mk (pullback.snd g.hom f.hom) = x}, ∃ x, y = f_1.hom x := by
+  obtain ⟨g, g_in_U, x, f_y_is_g_x⟩ := U_jointly_surjective (f.hom y)
+  use Over.mk <| pullback.snd g.hom f.hom
+  simp only [Functor.id_obj, Functor.const_obj_obj, Set.mem_setOf_eq, Over.mk_left, Over.mk_hom]
+  let mk_pb : X ⟶ Limits.pullback g.hom f.hom := pullback.lift (Function.const _ x)
+    (Function.const _ y) (by
+      ext
+      exact f_y_is_g_x.symm
+    )
+  constructor
+  · use g
+  use mk_pb (f.hom y)
+  simp [mk_pb]
+
+instance instSite.{u} : Site (Type u) where
   coverings := coverings
   iso := iso
   trans {X} U U_jointly V x := by
     simp only [Functor.const_obj_obj, Functor.id_obj, exists_prop,
       Set.mem_setOf_eq, ↓existsAndEq, and_true, Over.mk_left, Over.mk_hom, types_comp_apply]
     exact trans U U_jointly V x
-  pullback {X} f U U_jointly_surjective y := by
-    obtain ⟨g, g_in_U, x, f_y_is_g_x⟩ := U_jointly_surjective (f.hom y)
-    use Over.mk <| pullback.snd g.hom f.hom
-    simp only [Functor.id_obj, Functor.const_obj_obj, Set.mem_setOf_eq, Over.mk_left, Over.mk_hom]
-    let mk_pb : X ⟶ pullback g.hom f.hom := @pullback.lift _ _ X _ _ _ g.hom f.hom _
-      (Function.const _ x) (Function.const _ y) (by
-        ext
-        exact f_y_is_g_x.symm
-      )
-    constructor
-    · use g
-    use mk_pb (f.hom y)
-    simp [mk_pb]
-}
+  pullback := pullback
 
 end JointlySurjective
